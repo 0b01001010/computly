42c3f7eec19a0fd2858bb840437ec87fef897cd5
Amr <akkk33@protonmail.com>
2022-02-07 15:44:04 +0200

    Added basic styling to post

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -20,17 +20,29 @@
 </script>
 
 <script lang="ts">
+	import { theme as themeStore } from '$lib/stores/theme';
 	import type { Post } from '$lib/types/post';
 	export let page;
-	console.log(page);
 
 	export let post: Post;
+	$: darkMode = $themeStore === 'dark';
 </script>
 
 <svelte:head>
 	<title>{post.title}</title>
 </svelte:head>
-<article>
+<article class="{darkMode ? 'bg-dark' : 'bg-light'} border border-2">
+	<h1>{post.title}</h1>
 	<!-- {@html post.body} -->
 	<svelte:component this={page} />
 </article>
+
+<style lang="scss">
+	article {
+		padding: 1em;
+		margin: 2rem 1rem;
+		h1:first-child {
+			text-align: center;
+		}
+	}
+</style>
