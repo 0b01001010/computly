f5eb7bdad58d301fb94909e60c1cfa9d4f665940
Amr <akkk33@protonmail.com>
2022-02-07 13:18:16 +0200

    Re added mdsvex with svelte-add

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
+import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex';
 import { imagetools } from 'vite-imagetools';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,7 +15,8 @@ const config = {
 			scss: {
 				prependData: "@import 'src/lib/styles/variables.scss';"
 			}
-		})
+		}),
+		mdsvex(mdsvexConfig)
 	],
 
 	kit: {
