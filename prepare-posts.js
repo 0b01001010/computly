cd6c39358d478b46f93d2d144f8b491326a085b5
Amr <akkk33@protonmail.com>
2022-02-09 22:26:57 +0200

    Updated prepare-posts script to latest rules

--- prepare-posts.js
+++ prepare-posts.js
@@ -16,40 +16,26 @@ const extractImageMeta = async (source) => {
 	const [metadataResult, placeholder] = await Promise.all(metaPromises);
 
 	const { format, width, height } = metadataResult;
-
 	return { placeholder, format, width, height };
 };
 
-const moveAllImagesToStatic = async (post) => {
-	// Posts are now in /static/blog/posts/
-	// So this function will do different job
-	// Which is to return a list of all images in post's directory
-	// So they can be converted later
+const getPostImages = async (post) => {
+	/**
+	 * Get all images used in a post
+	 * @param {Object} post - Post object
+	 * @returns {Array} allImages - Array of image paths
+	 */
 	const { slug } = post;
-	const dstDir = path.join(BLOG_PATH, slug);
+	const postDir = path.join(BLOG_PATH, slug);
 
-	// Check if directory
-	if (!fs.existsSync(dstDir)) {
-		await makeDirectory(dstDir);
-	}
+	const _postFiles = fs.readdirSync(postDir);
 
-	const allImages = [];
-
-	const _postFiles = fs.readdirSync(path.join(BLOG_PATH, slug));
-	const _ogImages = _postFiles.filter((_file) => !_file.endsWith('md'));
-
-	_ogImages.forEach((_img) => {
-		const _imgPath = path.join(BLOG_PATH, slug, _img);
-		const _imgDst = path.join(dstDir, _img);
-
-		fs.copyFile(_imgPath, _imgDst, (err) => {
-			if (err) {
-				console.error(`Couldn't copy image ${_imgPath}`, err);
-			}
-		});
-		allImages.push(_imgDst);
+	const excludedExtensions = ['.md', '.json', '.avif', '.webp'];
+	const postImages = _postFiles.filter((_file) => {
+		const fileExtension = path.extname(_file);
+		return !excludedExtensions.includes(fileExtension);
 	});
-	return allImages;
+	return postImages;
 };
 
 const main = async () => {
@@ -67,16 +53,14 @@ const main = async () => {
 		}
 	});
 	const imgMetadata = await Promise.all(imgMetaPromises);
-
 	const ogImgFilesPromises = posts.map(async (_Post) => {
 		try {
-			return moveAllImagesToStatic(_Post);
+			return getPostImages(_Post);
 		} catch (err) {
 			console.error('Error while preparing image path: ', err);
 		}
 	});
 	const ogImgFiles = await Promise.all(ogImgFilesPromises);
-
 	posts.forEach((element, index) => {
 		const { slug, mainImage, mainImageAlt } = element;
 		const src = path.join(BLOG_PATH, slug, mainImage);
@@ -87,9 +71,8 @@ const main = async () => {
 		_postImages.forEach(async (_imgSrc) => {
 			try {
 				await generateFormats({
-					imgSrc: path.join(rootDir, _imgSrc),
-					size: Math.min(width, maxWidth),
-					outputPath: path.join(rootDir, _imgSrc)
+					imgSrc: path.join(BLOG_PATH, slug, _imgSrc),
+					size: Math.min(width, maxWidth)
 				});
 			} catch (error) {
 				console.error(
