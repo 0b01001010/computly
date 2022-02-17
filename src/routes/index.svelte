839f97ec64d216eec4fe99c6ea501a95b6df1ebe
Amr <akkk33@protonmail.com>
2022-02-17 04:10:30 +0200

    Playing around with three.js using a static background image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-
+	import Background from '$lib/Components/Landpage/Background.svelte';
 	import * as THREE from 'three';
 	import * as TH from 'threlte';
-
-	let background;
-	// let scene = new THREE.Scene();
-	onMount(() => {
-		new THREE.TextureLoader().load('/textures/360-bridge.jpg', (loaded) => {
-			background = loaded;
-			background.mapping = THREE.EquirectangularReflectionMapping;
-			background.encoding = THREE.sRGBEncoding;
-		});
-		// scene.background = new THREE.TextureLoader().load('./textures/360-bridge.jpg');
-	});
+	let cameraX = 0;
+	let cameraY = 0;
+	let cameraZ = 0;
 </script>
 
 <svelte:head>
@@ -21,10 +13,11 @@
 </svelte:head>
 <div>
 	<TH.Canvas>
-		<TH.PerspectiveCamera position={{ x: 19, y: 8, z: 10 }} fov={60}>
-			<TH.OrbitControls autoRotate enableZoom={false} maxPolarAngle={1.7} />
+		<TH.PerspectiveCamera position={{ x: 0, y: 0, z: 20 }} fov={60}>
+			<TH.OrbitControls maxPolarAngle={1.5} minPolarAngle={0.9} minAzimuthAngle={1} />
+			<Background />
 		</TH.PerspectiveCamera>
-		<TH.DirectionalLight shadow color={'white'} position={{ x: -15, y: 45, z: 20 }} />
+		<TH.DirectionalLight shadow color={'white'} position={{ x: -7, y: 18, z: 2 }} />
 		<TH.HemisphereLight skyColor={'white'} groundColor={'#ac844c'} intensity={0.4} />
 
 		<TH.Mesh
@@ -37,11 +30,40 @@
 			receiveShadow
 			position={{ y: -0.5 }}
 			rotation={{ x: -90 * (Math.PI / 180) }}
-			geometry={new THREE.CircleBufferGeometry(3, 72)}
-			material={new THREE.MeshStandardMaterial({ color: 'white' })}
+			geometry={new THREE.CircleBufferGeometry(5, 360)}
+			material={new THREE.MeshStandardMaterial({ color: 'white', side: THREE.DoubleSide })}
 		/>
-		<TH.Text text="Hi there" />
+		<TH.Text text="Hi there" color="#ff00000" position={{ x: 15, y: 2, z: 4 }} />
 	</TH.Canvas>
+</div>
+<div class="controls">
+	<input
+		type="range"
+		name="camX"
+		id="camX"
+		bind:value={cameraX}
+		min="-100"
+		max="100"
+		step="1"
+	/>{cameraX}
+	<input
+		type="range"
+		name="camY"
+		id="camY"
+		bind:value={cameraY}
+		min="-100"
+		max="100"
+		step="1"
+	/>{cameraY}
+	<input
+		type="range"
+		name="camZ"
+		id="camZ"
+		bind:value={cameraZ}
+		min="-100"
+		max="100"
+		step="1"
+	/>{cameraZ}
 </div>
 
 <style>
@@ -51,5 +73,21 @@
 		left: 0;
 		width: 100%;
 		height: 100%;
+	}
+	.controls {
+		position: fixed;
+		top: 0;
+		left: 0;
+		margin: 1rem;
+		padding: 0.5rem auto;
+		display: none;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: fit-content;
+		height: fit-content;
+		border-radius: 4px;
+		background: hsla(0, 0%, 100%, 0.5);
+		backdrop-filter: blur(10px);
 	}
 </style>
