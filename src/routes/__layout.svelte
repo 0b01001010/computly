4a6151d24d5a3e42ed1a421f42a1eac9961c7417
Amr <akkk33@protonmail.com>
2022-02-27 06:50:24 +0200

    Added contact info Images doesn't load in land page despite working in blog using the same component !!!!!!!! Added basic about info without testing because threlte is broken after last update :|

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+	import lazyload from 'vanilla-lazyload';
+	import { browser } from '$app/env';
+	// @ts-ignore
+	if (browser && !window.lazyloadInstance) {
+		// @ts-ignore
+		window.lazyloadInstance = new lazyload();
+	}
 </script>
 
 <slot />
